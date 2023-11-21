d3ed357cc87d873ca6a6642a439b3a4f5f0a9a00
waqasahmediqbal <73891669+waqasahmediqbal@users.noreply.github.com>
2023-11-21 21:18:36 +0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ connectDB();
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 //Init Middleware
 app.use(express.json({ extended: false }));
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
 const PORT = process.env.PORT || 5000;
 
 app.use("/api/user", require("./routes/api/users"));
