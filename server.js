3aab40af41b83c127686c756654b9cd759f83bd0
waqasahmediqbal <73891669+waqasahmediqbal@users.noreply.github.com>
2023-11-22 01:19:36 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,17 +2,14 @@ const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
 const path = require('path')
+const cors = require('cors');
 
+app.use(cors());
 connectDB();
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 //Init Middleware
 app.use(express.json({ extended: false }));
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
+
 const PORT = process.env.PORT || 5000;
 
 app.use("/api/user", require("./routes/api/users"));
